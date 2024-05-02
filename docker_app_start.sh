4b8ac8c1949a3ad45323939b894309cefd1ec317
Kourtney Reynolds <kourtneyreynolds@Kourtneys-MBP.xmission.com>
2024-05-02 10:30:05 -0600

    Docker changes to start and stop

--- docker_app_start.sh
+++ docker_app_start.sh
@@ -5,5 +5,5 @@ docker run \
 -p 3008:3008  \
 -v /Users/kourtneyreynolds/repos/backend-events-api-03b8ac7f82bd4ff9a91c35d630ca3626/:/app \
 --name event-service \
---network event-network \
+--network backend-events-api-03b8ac7f82bd4ff9a91c35d630ca3626_event-network \
 node-20-alpine:v1
---
+++ docker_app_stop.sh
@@ -0,0 +1,8 @@
+
+#! /bin/bash
+
+#stop container
+docker stop event-service
+
+#delete container
+docker container rm event-service
