6ef8720e9e15aad8daadea52618f13c3eb6f0fb7
Kourtney Reynolds <kourtneyreynolds@Kourtneys-MBP.xmission.com>
2024-05-05 14:01:08 -0600

    [BUG] Fix kafka environment so that the correct host name is being expose, ie the internal docker host and update the kafka broker

--- src/kafka.ts
+++ src/kafka.ts
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 
 export const kafkaStream = new Kafka({
     clientId: "create-event-publisher",
-    brokers: ["localhost:29092"],
+    brokers: ["host.docker.internal:29092"],
 });
   
 export const NEW_EVENT_TOPIC = 'new-event'
